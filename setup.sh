7c3df213c111be12c5bc45778867f0f3798b79a1
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-12-04 19:42:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 python3 -m venv myenv
 source myenv/bin/activate
-pip install -r requirements.txt
-sudo apt install bettercap
+pip3 install -r requirements.txt
+sudo apt install bettercap -y
 chmod +x src/listenOnSomeOne.py
+rm -rf src/__apache__
+rm -rf myenv
+PROCESSES=("bettercap" "ettercap" "wireshark" "etterfilter" "tshark")
+
+echo "[*] Checking and terminating sniffing tools if running..."
+
+for PROC in "${PROCESSES[@]}"; do
+    if pgrep -x "$PROC" >/dev/null; then
+        echo "[-] Killing running process: $PROC"
+        sudo killall -9 "$PROC"
+    else
+        echo "[+] $PROC is not running."
+    fi
+done
 clear
 sudo python3 src/listenOnSomeOne.py -h
 python3 src/process_pcap.py
