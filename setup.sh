421117b46070757669188f61d87d18f6852641c2
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-12-05 11:01:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ source myenv/bin/activate
 pip3 install -r requirements.txt
 sudo apt install bettercap -y
 chmod +x src/listenOnSomeOne.py
-rm -rf src/__apache__
+rm -rf src/__pycache__
 PROCESSES=("bettercap" "ettercap" "wireshark" "etterfilter" "tshark")
 
 echo "[*] Checking and terminating sniffing tools if running..."
