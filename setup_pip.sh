be187eb982910a605f23812903272620454b94e7
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-12-19 21:40:45 +0100

    Update setup_pip.sh

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+echo '#!/bin/bash                                                                   
 python3 -m venv myvenv;source myvenv/bin/activate
-pip install stealth-chopper
-alias stealth-chopper="sudo /home/$(whoami)/Desktop/StealthChopper/myvenv/bin/stealth-chopper"
-alias stealth-chopper-pcap="/home/$(whoami)/Desktop/StealthChopper/myvenv/bin/stealth-chopper-pcap"
+pip install stealth-chopper                          
+alias stealth-chopper="sudo myvenv/bin/stealth-chopper"
+alias stealth-chopper-pcap="myvenv/bin/stealth-chopper-pcap"
+clear
+stealth-chopper                  
+stealth-chopper-pcap -h    
+stealth-chopper-pcap'  > setup_pip.sh; chmod +x setup_pip.sh; source ./setup_pip.sh
