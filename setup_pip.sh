2029498a80bc778d54c547641ac074145f5a3851
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2025-12-27 18:29:08 +0100

    Remove redundant alias for stealth-chopper-pcap

--- setup_pip.sh
+++ setup_pip.sh
@@ -2,7 +2,6 @@
 python3 -m venv myvenv;source myvenv/bin/activate
 pip install stealth-chopper                          
 alias stealth-chopper="sudo myvenv/bin/stealth-chopper"
-alias stealth-chopper-pcap="myvenv/bin/stealth-chopper-pcap"
 clear
 stealth-chopper                  
 stealth-chopper-pcap -h    
