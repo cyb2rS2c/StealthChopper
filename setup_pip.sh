4658e611bebbe8eb48b3839ca4f0a7d257acf93e
cyb2rS2c <196449528+cyb2rS2c@users.noreply.github.com>
2026-03-15 20:00:52 +0100

    Fix alias for stealth-chopper-webcap script
    Updated alias for stealth-chopper-webcap to point to the correct script location.

--- setup_pip.sh
+++ setup_pip.sh
@@ -3,7 +3,7 @@ python3 -m venv myvenv;source myvenv/bin/activate
 sudo apt install bettercap -y
 pip install stealth-chopper                          
 alias stealth-chopper="sudo myvenv/bin/stealth-chopper"
-alias stealth-chopper-webcap="streamlit run myvenv/bin/stealth-chopper-webcap"
+alias stealth-chopper-webcap="streamlit run myvenv/lib/python3.13/site-packages/stealth_chopper/pcap_web.py"
 clear
 stealth-chopper                  
 stealth-chopper-pcap -h    
